ea20c6ba12b742351454e8c03a24fc0f1e1776f0
Ryan Leonard <rleonard772@gmail.com>
2022-05-20 13:30:43 -0400

    update

--- script.js
+++ script.js
@@ -16,159 +16,92 @@ function computerPlay(){
     }
 }
 
-
-
-
-
 const computerSelection = computerPlay();
-console.log(computerSelection);
 
-function playRound(){
-    const string = prompt("Rock, Paper, or Scissors?");
-    //console.log(string);
-    const array = string.split(" ")
-    for (let i =0; i<array.length; i++){
-        array[i]=array[i].charAt(0).toUpperCase() + array[i].toLowerCase(1).slice(1);
-    }
-    let playerSelection = array.join(" ");
-    
-    if (playerSelection === "Rock"){
-        if (computerSelection === "Paper"){
-            gameStatus = 2;
-            return "You Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock"){
+        if(computerSelection == "Paper"){
+            compScore++;
+        }
+
+        if(computerSelection == "Scissors"){
+            playerScore++;
             
         }
-        else if (computerSelection === "Scissors"){
-            gameStatus = 1;
-            return "You Win! Rock beats Scissors";
+
+        if(computerSelection == "Rock"){
+           
         }
-        else if (computerSelection ==="Rock"){
-            gameStatus = 3;
-            return "It's a Tie!";
-        }
+        
     }
-    if (playerSelection === "Paper"){
-        if (computerSelection === "Scissors"){
-            gameStatus = 2;
-            return "You Lose! Scissors beats Paper"
+
+    if(playerSelection == "scissors"){
+        if(computerSelection == "Paper"){
+            playerScore++;
+            return "You Win! Scissors beats Paper"
         }
-        else if (computerSelection === "Rock"){
-            gameStatus = 1;
-            return "You Win! Paper beats Rock";
+
+        if(computerSelection == "Scissors"){
+            return "Tie! You both chose scissors"
         }
-        else if (computerSelection ==="Paper"){
-            gameStatus = 3;
-            return "It's a Tie!";
+
+        if(computerSelection == "Rock"){
+            compScore++;
+            return "You lose! Rock beats scissors"
         }
+        
     }
-    if (playerSelection === "Scissors"){
-        if (computerSelection === "Paper"){
-            gameStatus = 1;
-            return "You Win! Scissors beats Paper";
+
+    if(playerSelection == "paper"){
+        if(computerSelection == "Paper"){
+            return "Tie! you both chose paper!"
         }
-        else if (computerSelection === "Rock"){
-            gameStatus = 2;
-            return "You Lose! Rock beats Scissors";
+
+        if(computerSelection == "Scissors"){
+            compScore++;
+            return "You lose! Scissors beats paper"
         }
-        else if (computerSelection ==="Scissors"){
-            gameStatus = 3;
-            return "It's a Tie!";
+
+        if(computerSelection == "Rock"){
+            playerScore++;
+            return "You win! Paper beats rock."
         }
+        
+    }
+
+
+    
+    
+}
+
+
+
+function game() {
+    
+
+    for(let i = 0; i < 5 ; i++){
+        const stringInput = prompt("Rock, Paper, or Scissors?");
+        const playerSelection = stringInput.toLowerCase();
+        console.log("Player choice: " + playerSelection);
+        console.log("Comp choice: " + computerSelection);
+
+        playRound(playerSelection, computerSelection)
+
+        console.log("Player score: " + playerScore + " | " + "Comp score: " + compScore);
+    }
+
+    if(playerScore > compScore){
+        return "You Win!"
+    }
+
+    if(compScore > playerScore){
+        return "You Lose :("
+    }
+
+    if (compScore == playerScore){
+        return "Tie"
     }
 }
 
-function game(){
-        // Round 1
-        window.alert(playRound());
-        if (gameStatus === 1){
-            playerScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            
-        }else if (gameStatus === 2) {
-            compScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            
-        }else if (gameStatus === 3){
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            
-        }
-        
-        //Round 2
-        window.alert(playRound());
-        if (gameStatus === 1){
-            playerScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            
-        }else if (gameStatus === 2) {
-            compScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-        }else if (gameStatus === 3){
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-        }
-        //Round 3
-        window.alert(playRound());
-        if (gameStatus === 1){
-            playerScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-               
-        }else if (gameStatus === 2) {
-            compScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-               
-        }else if (gameStatus === 3){
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-        }
-        //Round 4
-        window.alert(playRound());
-        if (gameStatus === 1){
-            playerScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            
-        }else if (gameStatus === 2) {
-            compScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-        
-        }else if (gameStatus === 3){
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-        }
-        //Round 5
-        window.alert(playRound());
-        if (gameStatus === 1){
-            playerScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            if (playerScore > compScore){
-                alert ("You Win! Congrats!");
-            }else if (compScore > playerScore){
-                alert ("You lose! Too bad");
-            }else if (playerScore === compScore) {
-                    alert("It's a tie! How boring");
-            }
-            
-        }else if (gameStatus === 2) {
-            compScore++
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            if (playerScore > compScore){
-                alert ("You Win! Congrats!");
-            }else if (compScore > playerScore){
-                alert ("You lose! Too bad");
-            }else if (playerScore === compScore) {
-                    alert("It's a tie! How boring");
-            }
-            
-        }else if (gameStatus === 3){
-            console.log("Your Score: " + playerScore, "CPU Score: " + compScore);
-            if (playerScore > compScore){
-                alert ("You Win! Congrats!");
-            }else if (compScore > playerScore){
-                alert ("You lose! Too bad");
-            }else if (playerScore === compScore) {
-                    alert("It's a tie! How boring");
-            }
-            
-        }
+console.log(game());
     
-
-}
-
-game();
-
